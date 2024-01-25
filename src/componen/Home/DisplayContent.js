780dcc80fd562230505a622455b724e526ebc5e5
Manase44 <129208497+Manase44@users.noreply.github.com>
2024-01-25 21:52:01 +0300

    commit1

---
+++ src/componen/Home/DisplayContent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const DisplayContent = () => {
+  return (
+    <div className="row" id="contentDiv">
+      <div className="col-md-6 col-lg-5">
+        <img src="/FullPic.png" alt="My" />
+      </div>
+      <div className="col-md-6">
+        hello
+      </div>
+    </div>
+  );
+};
+
+export default DisplayContent;
